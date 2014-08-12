e0509cc8e95acba2dd7fc9cdd14d2e62057c0b8c
eschoppik <elie@knowyo.com>
2014-08-12 16:08:21 -0700

    Update 1-temp.rb

--- 1-temp.rb
+++ 1-temp.rb
@@ -8,14 +8,14 @@ end
 
 def convert_temp
 	puts("type 1 to convert from Celsius to Farenheit OR type 2 to convert from Farenheit to Celsius")
-	type = gets.chomp().to_i
+	type = gets.chomp.to_i
 	if type == 1
 		puts("Enter a temperature in Celsius ")
-		temp = gets.chomp().to_f
+		temp = gets.chomp.to_f
 		puts "Your temperature in farenheit is #{celsius_to_farenheit(temp).round(2)}"
 	elsif type == 2
 		puts("Enter a temperature in Farenheit")
-		temp = gets.chomp().to_f
+		temp = gets.chomp.to_f
 		puts "Your temperature in celsius is #{farenheit_to_celsius(temp).round(2)}"
 	end
 end
